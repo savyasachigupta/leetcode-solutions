10384f09569817d510b38e221ab1fb3f974b8c3c
Savyasachi Gupta <ssg29may31dec@gmail.com>
2026-09-21 22:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (62.65%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
+}
